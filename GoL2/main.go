6ac7d302e92a5c974808eab351cf77aefe5c52d5
1renata11 <renata3646@icloud.com>
2025-08-06 14:52:10 +0500

    Добавлено задание 9

--- GoL2/main.go
+++ GoL2/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"GoL2/ntp_time"
-	"fmt"
+	"GoL2/unpacking"
 )
 
 func main() {
-	fmt.Println(ntp_time.CurrentTime())
+	unpacking.TestAll()
 }
--- GoL2/ntp_time/ntp_time.go
+++ GoL2/ntp_time/ntp_time.go
@@ -1,4 +1,4 @@
-package main
+package ntp_time
 
 import (
 	"fmt"
